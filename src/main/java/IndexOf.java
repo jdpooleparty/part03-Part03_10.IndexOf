866287428659453e5be8e67926e05db6d1c0569e
Jonathan Poole <jdpooleparty@gmail.com>
2022-06-18 21:38:31 -0700

    update

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -10,19 +10,20 @@ public class IndexOf {
         ArrayList<Integer> list = new ArrayList<>();
         int counter = 1;
         while (counter <=10) {
-            //int input = Integer.valueOf(scanner.nextLine());
-            int input = counter;
-            if (input == 11) {
+            int input = Integer.valueOf(scanner.nextLine());
+            //int input = counter;
+            if (input == -1) {
                 break;
             }
 
             list.add(input);
-            counter++;
+            
         }
-        System.out.println("These are the numbers in the ArrayList:");
-        for (int i = 0; i < list.size(); i++) {
-            int number = list.get(i);
-            System.out.println(number);
+        //System.out.println("This is the size of the ArrayList: " + list.size());
+        //System.out.println("These are the numbers in the ArrayList:");
+        //for (int i = 0; i < list.size(); i++) {
+        //    int number = list.get(i);
+        //    System.out.println(number);
 
         //for int values in list{}
         //System.out.println(list.get(0));
@@ -30,10 +31,11 @@ public class IndexOf {
         // implement here finding the indices of a number
         System.out.println("Search for?");
         int index_var = Integer.valueOf(scanner.nextLine());
+        //int index_var = 5;
 
         for (int output : list){
-            if (output == list.get(index_var)) {
-                System.out.println(index_var + " is at index "  + list.get(index_var));
+            if (output == index_var) {
+                System.out.println(index_var + " is at index " + list.indexOf(output));
             }
 
         }
